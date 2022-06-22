10dd2f2b7f998e488a7cab57193f932397cb9476
hatice <haticekatranci@yandex.com>
2022-06-22 14:47:02 +0300

    using-component-vuejs-main
    using-component-vuejs-main

---
+++ using-component-vuejs-main/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="container">
+  <div class="row">
+    <div class="col-md-6 col-md-offset-4">
+      <app-memory>
+        <h1 slot="heading">Güzel bir gün...</h1>
+        <p slot="content">Bu çok güzel bir anıydı...  </p>
+      </app-memory>
+    </div>
+  </div>
+</div>
+ 
+</template>
+
+<script>
+import Memory from "./Memory.vue";
+
+export default {
+  components : {
+    appMemory : Memory 
+  }
+}
+
+
+
+
+</script>
+
+<style>
+
+</style>
---
+++ using-component-vuejs-main/src/Memory.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="memory">
+      <slot name="heading"></slot>
+    </div>
+    <div class="memory">
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+div.memory {
+  border: 1px solid #ccc;
+  box-shadow: 1px 1px 2px #666;
+  margin-top: 20px;
+  padding: 20px 20px;
+}
+</style>
