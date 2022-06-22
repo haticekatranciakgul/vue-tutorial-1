34861ca7ba61c843b7f70b6fa01adea7e5ee683c
hatice <haticekatranci@yandex.com>
2022-06-22 14:43:10 +0300

    odevgelismiscomponentkullanimi-main
    odevgelismiscomponentkullanimi-main

---
+++ odevgelismiscomponentkullanimi-main/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <br>
+                <button @click="selectedComponent = 'appBlue'" class="btn btn-primary">Mavi Temayı Yükle</button>
+                <button @click="selectedComponent = 'appGreen'" class="btn btn-success">Yeşil Temayı Yükle</button>
+                <button @click="selectedComponent = 'appBlue'" class="btn btn-danger">Kırmızı Temayı Yükle </button>
+                <hr>
+                <component :is="selectedComponent">
+                    <h3> {{ selectedComponent }}  </h3>
+                    <p>İçerik...</p>
+                </component>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Blue from './components/Blue';
+    import Green from './components/Green';
+    import Red from './components/Red';
+
+    export default {
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        },
+        data : function() {
+           return {
+                selectedComponent : "appBlue",
+
+           }
+        },
+    }
+</script>
+
+<style>
+</style>
