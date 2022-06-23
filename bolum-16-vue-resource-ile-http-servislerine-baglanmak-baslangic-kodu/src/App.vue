100b3d03f986bd9e969d8f013fdb82e631998276
hatice <haticekatranci@yandex.com>
2022-06-23 10:15:25 +0300

    vue-resource-ile-http-servislerine-baglanmak-baslangic-kodu
    vue-resource-ile-http-servislerine-baglanmak-baslangic-kodu

---
+++ bolum-16-vue-resource-ile-http-servislerine-baglanmak-baslangic-kodu/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <h3>Vue-Resource</h3>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
